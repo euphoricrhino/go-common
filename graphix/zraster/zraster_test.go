2d971fa9ed6087b5399b7c36edadde3ef4895d67
Euphoricrhino <xinlaini@gmail.com>
2024-05-30 21:09:45 +0800

    zraster test

--- graphix/zraster/zraster_test.go
+++ graphix/zraster/zraster_test.go
@@ -1,7 +1,7 @@
 package zraster
 
 import (
-	"fmt"
+	"bytes"
 	"image/color"
 	"image/png"
 	"math"
@@ -12,9 +12,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func zrasterTestHelper(t *testing.T, paths []*SpacePath, benchmarkFile string) {
+	img := Run(Options{
+		Camera: graphix.NewCamera(
+			graphix.NewViewTransform(graphix.NewVec3(0, 0, 8), graphix.NewVec3(0, 0, -1), graphix.NewVec3(0, 1, 0)),
+			graphix.NewOrthographic(),
+			graphix.NewScreen(800, 800, -6, -6, 6, 6),
+		),
+		Paths:   paths,
+		Workers: 1,
+	})
+
+	benchmarkBytes, err := os.ReadFile(benchmarkFile)
+	assert.NoError(t, err)
+
+	buf := bytes.NewBuffer(nil)
+	assert.NoError(t, png.Encode(buf, img))
+	assert.Equal(t, benchmarkBytes, buf.Bytes())
+}
+
+// Three lines, with cyclic overlapping relationship.
+// red over blue, blue over green, green over red.
 func TestZRasterRunTriangle(t *testing.T) {
-	// Three lines, with cyclic overlapping relationship.
-	// red over blue, blue over green, green over red.
 	paths := []*SpacePath{
 		{
 			Segments: []*SpaceVertex{{
@@ -41,33 +60,18 @@ func TestZRasterRunTriangle(t *testing.T) {
 			LineWidth: 7,
 		},
 	}
-	img := Run(Options{
-		Camera: graphix.NewCamera(
-			graphix.NewViewTransform(graphix.NewVec3(0, 0, 8), graphix.NewVec3(0, 0, -1), graphix.NewVec3(0, 1, 0)),
-			graphix.NewOrthographic(),
-			graphix.NewScreen(800, 800, -6, -6, 6, 6),
-		),
-		Paths:   paths,
-		Workers: 1,
-	})
 
-	f, err := os.CreateTemp(os.TempDir(), "triangle*.png")
-	assert.NoError(t, err)
-	defer f.Close()
-	assert.NoError(t, png.Encode(f, img))
-	fmt.Fprintln(os.Stdout, f.Name())
+	zrasterTestHelper(t, paths, "testdata/triangle.png")
 }
 
 // This test shows that the alpha of the internal vertices of a path is not double counted.
 // The generated image should not have brighter spots at the internal vertices of the spiral path.
 func TestZRasterRunSpiral(t *testing.T) {
 	var segments []*SpaceVertex
-	dl := 2.0
+	dl := .1
 	dtheta := 5 * math.Pi / 180
-	color := graphix.RandColor()
-	// Set an non-opaque transparency.
-	color.A = 0xff
-	l := 10.0
+	color := color.NRGBA{R: 0, G: 0, B: 0xff, A: 0xff}
+	l := .5
 	theta := 0.0
 	for i := 0; i < 40; i++ {
 		l += dl
@@ -82,18 +86,27 @@ func TestZRasterRunSpiral(t *testing.T) {
 	theta += dtheta
 	paths := []*SpacePath{{Segments: segments, End: graphix.NewVec3(l*math.Cos(theta), l*math.Sin(theta), 0), LineWidth: 15}}
 
-	img := Run(Options{
-		Camera: graphix.NewCamera(
-			graphix.NewViewTransform(graphix.NewVec3(0, 0, 8), graphix.NewVec3(0, 0, -1), graphix.NewVec3(0, 1, 0)),
-			graphix.NewOrthographic(),
-			graphix.NewScreen(800, 800, -100, -100, 100, 100),
-		),
-		Paths:   paths,
-		Workers: 1,
-	})
-	f, err := os.CreateTemp(os.TempDir(), "spiral*.png")
-	assert.NoError(t, err)
-	defer f.Close()
-	assert.NoError(t, png.Encode(f, img))
-	fmt.Fprintln(os.Stdout, f.Name())
+	zrasterTestHelper(t, paths, "testdata/spiral.png")
+}
+
+// This test shows a path with three segments: AB-BC-CD, where the projection of AB and CD intersect.
+// Their projected intersection should have a brighter color due to the alpha blending (at different z),
+// but B and C should not because they belong to adjacent strokes.
+func TestZRasterRunSelfIntersectingPath(t *testing.T) {
+	color := color.NRGBA{R: 0, G: 0xff, B: 0, A: 0x80}
+	paths := []*SpacePath{{
+		Segments: []*SpaceVertex{{
+			Pos:   graphix.NewVec3(-5, -5, -5),
+			Color: color,
+		}, {
+			Pos:   graphix.NewVec3(5, 5, 5),
+			Color: color,
+		}, {
+			Pos:   graphix.NewVec3(-5, 5, -5),
+			Color: color,
+		}},
+		End:       graphix.NewVec3(5, -5, -5),
+		LineWidth: 15,
+	}}
+	zrasterTestHelper(t, paths, "testdata/self-intersecting.png")
 }
